79672da34cfe59e8f52c8bee6130f76a67b72c8c
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2021-08-02 23:57:15 +0200

    add client side

---
+++ client/src/components/partials/Nav.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container-fluid">
+    <b-navbar toggleable="lg" variant="white" type="white">
+      <b-navbar-brand tag="h1" class="mb-0" href="/" >Artisan</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item href="/about">About</b-nav-item>
+          <b-nav-item href="#">Create New Event</b-nav-item>
+        </b-navbar-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ms-auto">
+          <b-nav-item href="#">Sign In</b-nav-item>
+          <b-nav-item href="#">Sign Up</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Header />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from "@/components/partials/Header.vue";
+export default {
+  name: "Home",
+  components: {
+    Header,
+  },
+};
+</script>
