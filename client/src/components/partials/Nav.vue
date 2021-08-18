934afd295ca0084ac49e1421ff4f67e18a8ef5a9
Frances Joffany Navarro <francesjoffanynavarro@gmail.com>
2021-08-18 23:53:26 +0200

    change the font andadd margin to navbar

--- client/src/components/partials/Nav.vue
+++ client/src/components/partials/Nav.vue
@@ -15,14 +15,14 @@
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav>
-          <b-nav-item href="/about">About</b-nav-item>
-          <b-nav-item href="#">Create New Event</b-nav-item>
+          <b-nav-item href="/about" class="m-2">About</b-nav-item>
+          <b-nav-item href="#" class="m-2">Create New Event</b-nav-item>
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ms-auto">
-          <b-nav-item href="#">Sign In</b-nav-item>
-          <b-nav-item href="#">Sign Up</b-nav-item>
+          <b-nav-item href="#" class="m-2">Sign In</b-nav-item>
+          <b-nav-item href="#" class="m-2">Sign Up</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
@@ -42,7 +42,7 @@ export default {};
   font-size: 50px;
 }
 ul li a {
-  font-family: "Lato", sans-serif;
+  font-family: "Open Sans", sans-serif;
 }
 
 .navbar-brand,
